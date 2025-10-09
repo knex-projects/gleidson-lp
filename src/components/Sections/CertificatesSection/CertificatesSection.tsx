593671a2ec83d8a0a30caaa1a3b0c676386d8936
Carlos H <www.carlos4k@gmail.com>
2025-10-09 15:53:13 -0300

    feat(CertificateSection): adicionar responsividade adequada para telas menores

--- src/components/Sections/CertificatesSection/CertificatesSection.tsx
+++ src/components/Sections/CertificatesSection/CertificatesSection.tsx
@@ -3,6 +3,11 @@ import Card from "../../ui/cardCertificate";
 import Overlay from "./Overlay";
 import { certificatesMock } from "../../../mock/certificatesMock";
 import type { Certificate } from "../../../mock/certificate";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 export default function CertificatesSection() {
   const [certificates, setCertificates] = useState<Certificate[]>([]);
@@ -13,15 +18,48 @@ export default function CertificatesSection() {
   }, []);
 
   return (
-    <section className="md:justify-center items-center mx-auto max-w-[1248px] grid gap-[24px] lg:[grid-template-columns:repeat(6,200px)] ml-4 md:ml-auto">
-      <h2 className="text-[#002050] text-[28px] md:text-[48px] col-span-4 md:col-span-6 md:mb-[36px]">
+    <section>
+      <div className="hidden md:block">
+        <DesktopCertificates
+          certificates={certificates}
+          selectedImages={selectedImages}
+          setSelectedImages={setSelectedImages}
+        />
+      </div>
+      <div className="md:hidden">
+        <MobileCertificates
+          certificates={certificates}
+          selectedImages={selectedImages}
+          setSelectedImages={setSelectedImages}
+        />
+      </div>
+    </section>
+  );
+}
+
+type DesktopCertificatesProps = {
+  certificates: Certificate[];
+  selectedImages: string[] | null;
+  setSelectedImages: React.Dispatch<React.SetStateAction<string[] | null>>;
+};
+
+function DesktopCertificates({
+  certificates,
+  selectedImages,
+  setSelectedImages,
+}: DesktopCertificatesProps) {
+  return (
+    <section className="justify-center items-center mx-auto max-w-[1248px] grid gap-[24px] lg:[grid-template-columns:repeat(6,200px)] mb-4 ml-auto">
+      <h2 className="text-[#002050] text-[48px] col-span-6 md:mb-[36px]">
         Certificados
       </h2>
-      <div className="w-[390px] md:mx-auto md:max-w-[1248px] grid gap-[12px] md:gap-[24px] lg:[grid-template-columns:repeat(6,200px)] carroussel">
+      <div className="w-[390px] mx-auto max-w-[1248px] grid gap-[24px] lg:[grid-template-columns:repeat(6,200px)]">
         {certificates.map((certificate) => (
-          <div className="col-span-4 md:col-span-2 flex justify-center">
+          <div
+            className="col-span-2 flex justify-center"
+            key={certificate.id}
+          >
             <Card
-              key={certificate.id}
               title={certificate.title}
               description={certificate.description}
               onClick={() => setSelectedImages(certificate.images)}
@@ -35,6 +73,40 @@ export default function CertificatesSection() {
           />
         )}
       </div>
+    </section>
+  );
+}
+
+function MobileCertificates({
+  certificates,
+  selectedImages,
+  setSelectedImages,
+}: DesktopCertificatesProps) {
+  return (
+    <section>
+      <h2 className="text-[#002050] text-[28px] md:text-[48px] mb-[36px]">
+        Certificados
+      </h2>
+      <Carousel className="w-[368px] h-auto pb-4 -ml-4 ">
+        <CarouselContent className="-ml-2">
+          {certificates.map((certificate) => (
+            <CarouselItem className="pl-2" key={certificate.id}>
+              <Card
+                title={certificate.title}
+                description={certificate.description}
+                onClick={() => setSelectedImages(certificate.images)}
+              />
+              <br />
+            </CarouselItem>
+          ))}
+          {selectedImages && (
+            <Overlay
+              images={selectedImages}
+              onClose={() => setSelectedImages(null)}
+            />
+          )}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 }
