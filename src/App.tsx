2670abb779f5c79b5b916fcd94156e95beb34922
Hércules Costa <dev.hrcules@gmail.com>
2025-11-11 14:53:37 -0300

    horfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/common/footer";
 import { Header } from "./components/common/header";
 import AboutSection from "./components/Sections/AboutSection/about-section";
 import CertificatesSection from "./components/Sections/CertificatesSection";
@@ -6,13 +7,16 @@ import PortfolioSection from "./components/Sections/PortfolioSection/potfolio";
 
 function App() {
   return (
-    <div className="flex items-center justify-center flex-col px-[10%] mt-5 py-6">
-      <Header />
-      <HeroSection />
-      <AboutSection />
-      <CertificatesSection />
-      <PortfolioSection />
-    </div>
+    <>
+      <div className="flex items-center justify-center flex-col px-[10%] mt-5 py-6">
+        <Header />
+        <HeroSection />
+        <AboutSection />
+        <CertificatesSection />
+        <PortfolioSection />
+      </div>
+      <Footer />
+    </>
   );
 }
 
